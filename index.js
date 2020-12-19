67bcf6da860fba87fc07ba7eff02b58cfbc657e1
andi2612 <76191064+andi2612@users.noreply.github.com>
2020-12-19 11:53:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,13 +12,12 @@ const menu = require("./lib/menu.js");
 const donate = require("./lib/donate.js");
 const info = require("./lib/info.js");
 //
-const BotName = 'ST4RZ BOT 🤖'; // Nama Bot Whatsapp
-const instagramlu = 'https://instagram.com/bintang_nur_pradana'; // Nama Instagramlu cok
-const whatsapplu = '0819-4698-3575'; // Nomor whatsapplu cok
+const BotName = 'nnndbot'; // Nama Bot Whatsapp
+const instagramlu = 'https://instagram.com/nnndiiii_'; // Nama Instagramlu cok
+const whatsapplu = '081-615-019-293'; // Nomor whatsapplu cok
 const kapanbotaktif = '24 Jam'; // Kapan bot lu aktif
-const grupch1 = 'https://chat.whatsapp.com/FsAlnxqz6y2BhCQi5ayCLG'; // OFFICIAL GRUP LU 1
-const grupch2 = 'https://chat.whatsapp.com/KLW3UlFfeaH36Ucm5zRfCz'; // OFFICIAL GRUP LU 2
-//
+const 
+const 
 const
 {
    WAConnection,
@@ -67,7 +66,7 @@ conn.on('user-presence-update', json => console.log(`[ ${moment().format("HH:mm:
 conn.on('message-status-update', json =>
 {
    const participant = json.participant ? ' (' + json.participant + ')' : '' // participant exists when the message is from a group
-   console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by @bintang_nur_pradana`)
+   console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by @nnndiiii_`)
 })
 
 conn.on('message-new', async(m) =>
